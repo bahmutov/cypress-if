9691d1966c14225b3257ebb3c95809f449cd480f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-28 14:09:53 -0600

    add a single assertion support

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,39 @@
-Cypress.Commands.add('if', { prevSubject: true }, function (subject) {
-  const cmd = cy.state('current')
-  console.log('if', cmd.attributes, 'subject', subject)
-  console.log('next command', cmd.next)
-  if (!subject && cmd.attributes.next) {
-    console.log(
-      'skipping the next command',
-      cmd.attributes.next.attributes.name,
-    )
-    cmd.attributes.next.attributes.skip = true
-    cy.log(`**skipping ${cmd.attributes.next.attributes.name}**`)
-  }
-  return subject
-})
+Cypress.Commands.add(
+  'if',
+  { prevSubject: true },
+  function (subject, assertion) {
+    const cmd = cy.state('current')
+    console.log('if', cmd.attributes, 'subject', subject)
+    console.log('next command', cmd.next)
+
+    const hasSubject = Boolean(subject)
+    let assertionsPassed = true
+    if (hasSubject && assertion) {
+      try {
+        expect(subject).to.be[assertion]
+      } catch (e) {
+        console.error(e)
+        assertionsPassed = false
+      }
+    }
+
+    if (!hasSubject || !assertionsPassed) {
+      if (cmd.attributes.next) {
+        console.log(
+          'skipping the next command',
+          cmd.attributes.next.attributes.name,
+        )
+        cmd.attributes.next.attributes.skip = true
+        cy.log(`**skipping ${cmd.attributes.next.attributes.name}**`)
+        if (subject) {
+          cy.wrap(subject)
+        }
+        return
+      }
+    }
+    return subject
+  },
+)
 
 Cypress.Commands.overwrite('get', function (get, selector) {
   // can we see the next command already?
@@ -50,4 +72,11 @@ it('finds the li elements', () => {
   cy.get('button#load').if().should('have.text', 'Load').click()
   // if the button exists, click on it
   cy.get('button#does-not-exist').if().click()
+})
+
+it('clicks on the button if it is visible', () => {
+  cy.visit('index.html')
+  cy.get('button#hidden').if('visible').click()
+  // but we can click on the visible button
+  cy.get('button#load').if('visible').click()
 })
