4a139543eefe272555d67a733fa01c82ffa39d56
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-08-30 14:29:01 -0600

    verify the dialog was submitted or not

--- cypress/e2e/close-dialog.cy.js
+++ cypress/e2e/close-dialog.cy.js
@@ -1,16 +1,24 @@
 /// <reference types="cypress" />
 import '../../src'
 
+function visit(showDialog = true) {
+  cy.visit('cypress/close-dialog.html', {
+    onBeforeLoad(win) {
+      // trick the app to open / hide the dialog
+      cy.stub(win.Math, 'random').returns(showDialog ? 0 : 1)
+    },
+  })
+  const submitForm = cy.stub().as('submitForm')
+  if (showDialog) {
+    cy.get('dialog#survey').invoke('on', 'submit', submitForm)
+  }
+}
+
 it(
   'closes the survey dialog',
   { viewportWidth: 500, viewportHeight: 500 },
   () => {
-    cy.visit('cypress/close-dialog.html', {
-      onBeforeLoad(win) {
-        // trick the app to open the dialog
-        cy.stub(win.Math, 'random').returns(0)
-      },
-    })
+    visit(true)
     cy.get('dialog#survey')
       .if('visible')
       .wait(1000)
@@ -19,6 +27,8 @@ it(
     // if there is a dialog on top,
     // then the main text is not visible
     cy.get('#main').should('be.visible')
+    // in this test the dialog should have been submitted
+    cy.get('@submitForm').should('have.been.calledOnce')
   },
 )
 
@@ -26,12 +36,7 @@ it(
   'skips the commands since the dialog is closed',
   { viewportWidth: 500, viewportHeight: 500 },
   () => {
-    cy.visit('cypress/close-dialog.html', {
-      onBeforeLoad(win) {
-        // trick the app to close the dialog
-        cy.stub(win.Math, 'random').returns(1)
-      },
-    })
+    visit(false)
     cy.get('dialog#survey')
       .if('visible')
       .wait(1000)
@@ -40,20 +45,60 @@ it(
     // if there is a dialog on top,
     // then the main text is not visible
     cy.get('#main').should('be.visible')
+    // in this test the dialog was never submitted
+    cy.get('@submitForm').should('not.have.been.called')
   },
 )
+
+describe('cy.then support', () => {
+  it(
+    'executes the .then callback if the dialog is visible',
+    { viewportWidth: 500, viewportHeight: 500 },
+    () => {
+      visit(true)
+      cy.get('dialog#survey')
+        .if('visible')
+        .then(() => {
+          cy.log('**closing the dialog**')
+          cy.contains('dialog#survey button', 'Close').wait(1000).click()
+          cy.get('dialog').should('not.be.visible')
+        })
+
+      // if there is a dialog on top,
+      // then the main text is not visible
+      cy.get('#main').should('be.visible')
+      // in this test the dialog should have been submitted
+      cy.get('@submitForm').should('have.been.calledOnce')
+    },
+  )
+
+  it(
+    'skips the .then callback if the dialog is hidden',
+    { viewportWidth: 500, viewportHeight: 500 },
+    () => {
+      visit(false)
+      cy.get('dialog#survey')
+        .if('visible')
+        .then(() => {
+          cy.log('**closing the dialog**')
+          cy.contains('dialog#survey button', 'Close').wait(1000).click()
+          cy.get('dialog').should('not.be.visible')
+        })
+      // if there is a dialog on top,
+      // then the main text is not visible
+      cy.get('#main').should('be.visible')
+      // in this test the dialog was never submitted
+      cy.get('@submitForm').should('not.have.been.called')
+    },
+  )
+})
 
 describe('cy.contains support', () => {
   it(
     'clicks the close survey button',
     { viewportWidth: 500, viewportHeight: 500 },
     () => {
-      cy.visit('cypress/close-dialog.html', {
-        onBeforeLoad(win) {
-          // trick the app to open the dialog
-          cy.stub(win.Math, 'random').returns(0)
-        },
-      })
+      visit(true)
       cy.contains('dialog#survey button', 'Close')
         .if('visible')
         .wait(1000)
@@ -61,6 +106,8 @@ describe('cy.contains support', () => {
       // if there is a dialog on top,
       // then the main text is not visible
       cy.get('#main').should('be.visible')
+      // in this test the dialog should have been submitted
+      cy.get('@submitForm').should('have.been.calledOnce')
     },
   )
 
@@ -68,12 +115,7 @@ describe('cy.contains support', () => {
     'skips click when the button is hidden',
     { viewportWidth: 500, viewportHeight: 500 },
     () => {
-      cy.visit('cypress/close-dialog.html', {
-        onBeforeLoad(win) {
-          // trick the app to hide the dialog
-          cy.stub(win.Math, 'random').returns(1)
-        },
-      })
+      visit(false)
       cy.contains('dialog#survey button', 'Close')
         .if('visible')
         .wait(1000)
@@ -81,6 +123,8 @@ describe('cy.contains support', () => {
       // if there is a dialog on top,
       // then the main text is not visible
       cy.get('#main').should('be.visible')
+      // in this test the dialog was never submitted
+      cy.get('@submitForm').should('not.have.been.called')
     },
   )
 })
