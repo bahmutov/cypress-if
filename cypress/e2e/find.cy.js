9a0aa4a0d8c65d57d8600bf38f31dd4c3543623c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-22 07:57:12 -0400

    fix: add cy.find + cy.if support (#33)
    * testing find command
    
    * fix: add cy.find command support

---
+++ cypress/e2e/find.cy.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+// @ts-check
+
+import '../../src'
+
+describe('cy.find', () => {
+  it('takes the if branch', () => {
+    cy.visit('cypress/checkbox.html')
+    cy.get('#app1')
+      .find('#enrolled')
+      .if('exist')
+      .then(cy.spy().as('if'))
+      .log('checkbox found')
+      .else()
+      .log('checkbox not found')
+      .then(cy.spy().as('else'))
+    cy.get('@if').should('have.been.called')
+    cy.get('@else').should('not.have.been.called')
+  })
+
+  it('takes the else branch', () => {
+    cy.visit('cypress/checkbox.html')
+    // the checkbox should not be checked
+    cy.get('#enrolled').should('not.be.checked')
+
+    cy.get('#app1')
+      .find('#enrolled')
+      .if('checked')
+      .then(cy.spy().as('if'))
+      .log('checkbox found')
+      .else()
+      .log('checkbox not found')
+      .then(cy.spy().as('else'))
+    cy.get('@else').should('have.been.called')
+    cy.get('@if').should('not.have.been.called')
+  })
+
+  // https://github.com/bahmutov/cypress-if/issues/32
+  it('checks if the element exists (it does not)', () => {
+    cy.visit('cypress/checkbox.html')
+    cy.get('#app1')
+      .find('#doest-not-exist')
+      .if('exist')
+      .then(cy.spy().as('if'))
+      .log('checkbox found')
+      .else()
+      .log('checkbox not found')
+      .then(cy.spy().as('else'))
+    cy.get('@else').should('have.been.called')
+    cy.get('@if').should('not.have.been.called')
+  })
+})
