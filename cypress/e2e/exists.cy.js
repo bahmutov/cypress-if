939ab7f1d7182f46c4dfa53c5e6c67e09a5bcd75
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-17 22:41:23 -0400

    fix: Subject does not exist assertion (#46)
    * add exist spec
    
    * fix: handle element not exist assertion

---
+++ cypress/e2e/exists.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+// @ts-check
+
+import '../../src'
+
+beforeEach(() => {
+  cy.visit('cypress/index.html')
+})
+
+it('checks an element that exists', () => {
+  cy.get('#fruits')
+    .if('exist')
+    .then(cy.spy().as('if'))
+    .else()
+    .then(cy.spy().as('else'))
+  cy.get('@if').should('have.been.calledOnce')
+  cy.get('@else').should('not.have.been.called')
+})
+
+it('checks an element that does not exists', () => {
+  cy.get('#not-found')
+    .if('exist')
+    .then(cy.spy().as('if'))
+    .else()
+    .then(cy.spy().as('else'))
+  cy.get('@else').should('have.been.calledOnce')
+  cy.get('@if').should('not.have.been.called')
+})
+
+// https://github.com/bahmutov/cypress-if/issues/45
+it('checks an element that does not exists using not.exist', () => {
+  cy.get('#not-found').should('not.exist')
+  cy.get('#not-found')
+    .if('not.exist')
+    .then(cy.spy().as('if'))
+    .else()
+    .then(cy.spy().as('else'))
+  cy.get('@if').should('have.been.calledOnce')
+  cy.get('@else').should('not.have.been.called')
+})
