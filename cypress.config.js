c10a1a6261a8d40dced2cdfdba5889c915099df2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-22 15:03:13 -0500

    bump to Cypress v11.2.0

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    experimentalRunAllSpecs: true,
     fixturesFolder: false,
     supportFile: false,
     viewportWidth: 200,
