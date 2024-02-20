2dcde995c7220ca7fb9c8358accd1eec3a99f624
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-20 16:53:30 -0500

    add if enabled example

---
+++ cypress/e2e/click-enabled.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+// @ts-check
+
+import '../../src'
+
+it('clicks the button if enabled', () => {
+  cy.visit('cypress/enabled-button.html')
+  cy.contains('button', 'Click Me')
+    // tests using Chai assertion "be.enabled"
+    .if('enabled')
+    .click()
+    .should('have.text', 'Clicked')
+    .else('Button is disabled')
+})
+
+it('clicks the button if enabled, checks using jQuery is :enabled', () => {
+  cy.visit('cypress/enabled-button.html')
+  cy.contains('button', 'Click Me')
+    .invoke('is', ':enabled')
+    .if('equals', true)
+    // grab the button again using "cy.document + cy.contains"
+    // combination to avoid using the Boolean subject
+    // from the previous command
+    .document()
+    .contains('button', 'Click Me')
+    .click()
+    .should('have.text', 'Clicked')
+    .else('Button is disabled')
+})
