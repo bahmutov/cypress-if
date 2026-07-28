b7857ac9ccfc950313dca6ba036841acfc8bc8fd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2026-07-28 11:47:35 -0400

    another example

--- cypress/e2e/items-load-with-error.cy.js
+++ cypress/e2e/items-load-with-error.cy.js
@@ -2,8 +2,72 @@
 // @ts-check
 
 import '../../src'
+import { recurse } from 'cypress-recurse'
 
-it('loads items', () => {
+it('loads items', { defaultCommandTimeout: 5000 }, () => {
   cy.visit('cypress/items-load-with-error.html')
   cy.contains('h1', 'Items load')
+  cy.get('#items li').should('have.length.greaterThan', 1)
 })
+
+it('loads items or shows error', { defaultCommandTimeout: 5000 }, () => {
+  cy.visit('cypress/items-load-with-error.html')
+  cy.contains('h1', 'Items load')
+
+  cy.depends({
+    '#items #error': new Error('Items failed to load'),
+    '#items #empty': new Error('Items are empty'),
+    '#items li': 'Items are loaded',
+  })
+
+  cy.get('#items li').should('have.length.greaterThan', 1)
+})
+
+it(
+  'loads items or shows error with retries',
+  { defaultCommandTimeout: 5000, retries: 2 },
+  () => {
+    cy.visit('cypress/items-load-with-error.html')
+    cy.contains('h1', 'Items load')
+
+    cy.depends({
+      '#items #error': new Error('Items failed to load'),
+      '#items #empty': new Error('Items are empty'),
+      '#items li': 'Items are loaded',
+    })
+
+    cy.get('#items li').should('have.length.greaterThan', 1)
+  },
+)
+
+it(
+  'loads items or shows error with local and test retries',
+  { defaultCommandTimeout: 5000, retries: 2 },
+  () => {
+    cy.visit('cypress/items-load-with-error.html')
+    cy.contains('h1', 'Items load')
+
+    recurse(
+      () =>
+        cy
+          .depends({
+            '#items #error': new Error('Items failed to load'),
+            '#items #empty': 'Items are empty',
+            '#items li': 'Items are loaded',
+          })
+          .its('selector'),
+      (selector) => selector === '#items li',
+      {
+        log: true,
+        timeout: 30_000,
+        limit: 5,
+        post() {
+          cy.log('Empty items, reloading the page')
+          cy.reload()
+        },
+      },
+    )
+
+    cy.get('#items li').should('have.length.greaterThan', 1)
+  },
+)
